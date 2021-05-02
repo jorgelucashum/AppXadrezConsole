663ff4d4201aac01249183c3e60fc2a596910805
Jorge <jorgelucashum@gmail.com>
2021-05-01 23:16:23 -0300

    Revert "Revert "Comecando a colocar pecas no tabuleiro""
    This reverts commit 4f2413e22db39f5d41d5ccb4c0f86265cfb6b0d5.

--- AppXadrezConsole/Program.cs
+++ AppXadrezConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using tabuleiro;
+using xadrez;
 
 namespace AppXadrezConsole
 {
@@ -9,6 +10,11 @@ namespace AppXadrezConsole
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8); // Instanciando o tabuleiro de tamanho 8x8.
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
             Tela.imprimirTabuleiro(tab);
         }
     }
