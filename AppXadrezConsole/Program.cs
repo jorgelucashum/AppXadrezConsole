843b9a947fc2c56ea92e7eeb2cb9897b52918aa9
Jorge <jorgelucashum@gmail.com>
2021-05-13 21:12:33 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- AppXadrezConsole/Program.cs
+++ AppXadrezConsole/Program.cs
@@ -9,13 +9,24 @@ namespace AppXadrezConsole
     {
         static void Main(string[] args)
         {
+            try
+            {
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                Tabuleiro tab = new Tabuleiro(8, 8); // Instanciando o tabuleiro de tamanho 8x8.
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
+                Tela.imprimirTabuleiro(tab);
+            }
+
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
 
             Console.ReadLine();
--- AppXadrezConsole/Tela.cs
+++ AppXadrezConsole/Tela.cs
@@ -11,6 +11,7 @@ namespace AppXadrezConsole
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(tab.linhas - i + "  "); // Enumarando as linhas ao lado.
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null) // Se a peça não tiver recebido um valor será mostrado um '-'.
@@ -19,12 +20,30 @@ namespace AppXadrezConsole
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + "  "); // Usando o método do objeto 'tab' para ter acesso as informações da 'peca'.
-                    } 
+                        imprimirPeca(tab.peca(i, j)); // Usando o método do objeto 'tab' para ter acesso as informações da 'peca'.
+                        Console.Write("  ");
+                    }            
                 }
                 Console.WriteLine(""); //Pular para a linha abaixo após percorrer toda a coluna de uma linha.
             }
+            Console.WriteLine("   a  b  c  d  e  f  g  h"); // Inserindo identificadores das colunas.
 
+
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
